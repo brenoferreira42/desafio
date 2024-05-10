0f1205c6ce944ad4fc24ad9ca2829628089e3d5a
brenofilipesilva <breno.silva@ilia.digital>
2024-05-10 01:14:28 -0300

    refactor: updating encapsulation of methods and attributes throughout the code

--- src/player/player_strategies.py
+++ src/player/player_strategies.py
@@ -5,12 +5,12 @@ class PlayerStrategy(object):
     def play(self, property, player):
         raise NotImplementedError("Method play must be implemented by its subclasses")
 
-    def buy_property(self, property, player):
+    def _buy_property(self, property, player):
         if property.owner is None:
             player.balance -= property.sell_value
             property.owner = player
 
-    def pay_rent(self, property, player):
+    def _pay_rent(self, property, player):
         if property.owner is not None:
             player.balance -= property.rent_value
             property.owner.balance += property.rent_value
@@ -20,27 +20,27 @@ class ImpulsivePlayerStrategy(PlayerStrategy):
     # NOTE: O jogador impulsivo compra qualquer propriedade sobre a qual ele parar.
     def play(self, property, player):
         if player.balance >= property.sell_value:
-            self.buy_property(property, player)
+            self._buy_property(property, player)
 
-        self.pay_rent(property, player)
+        self._pay_rent(property, player)
 
 
 class DemandingPlayerStrategy(PlayerStrategy):
     # NOTE: O jogador exigente compra qualquer propriedade, desde que o valor do aluguel dela seja maior do que 50.
     def play(self, property, player):
         if property.rent_value > 50 and player.balance >= property.sell_value:
-            self.buy_property(property, player)
+            self._buy_property(property, player)
 
-        self.pay_rent(property, player)
+        self._pay_rent(property, player)
 
 
 class CautiousPlayerStrategy(PlayerStrategy):
     # NOTE: O jogador cauteloso compra qualquer propriedade desde que ele tenha uma reserva de 80 saldo sobrando depois de realizada a compra.
     def play(self, property, player):
         if (player.balance - property.sell_value) >= 80 and player.balance >= property.sell_value:  # fmt: skip
-            self.buy_property(property, player)
+            self._buy_property(property, player)
 
-        self.pay_rent(property, player)
+        self._pay_rent(property, player)
 
 
 class RandomPlayerStrategy(PlayerStrategy):
@@ -49,6 +49,6 @@ class RandomPlayerStrategy(PlayerStrategy):
         buy_probability = random.random()
 
         if buy_probability < 0.5 and player.balance >= property.sell_value:
-            self.buy_property(property, player)
+            self._buy_property(property, player)
 
-        self.pay_rent(property, player)
+        self._pay_rent(property, player)
